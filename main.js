faa160c822ac8f19a6815d786122909c0835a249
Adam <elfaqiradam6@gmail.com>
2026-03-17 23:57:19 +0000

    Add smooth scroll and welcome message to main.js

---
+++ main.js
@@ -0,0 +1,7 @@
+// Smooth scroll to Projects section
+document.getElementById('explore-btn').addEventListener('click', () => {
+    document.getElementById('projects').scrollIntoView({ behavior: 'smooth' });
+});
+
+// Simple welcome console message
+console.log("%cWelcome to EvoTeam! Where code evolves 🧡", "color: orange; font-size: 16px; font-weight: bold;");
