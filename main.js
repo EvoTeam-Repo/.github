64cee1b41c63beadc20784e76bb6cec68ac87469
Adam <elfaqiradam6@gmail.com>
2026-03-18 00:07:02 +0000

    Delete main.js

--- main.js
+++
@@ -1,7 +0,0 @@
-// Smooth scroll to Projects section
-document.getElementById('explore-btn').addEventListener('click', () => {
-    document.getElementById('projects').scrollIntoView({ behavior: 'smooth' });
-});
-
-// Simple welcome console message
-console.log("%cWelcome to EvoTeam! Where code evolves 🧡", "color: orange; font-size: 16px; font-weight: bold;");
